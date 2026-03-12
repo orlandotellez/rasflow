96811e92c42fea0babddaa79cda6eaf8dfeea6c5
orlandotellez <espadachinkinno@gmail.com>
2026-03-11 18:37:14 -0600

    feat: agregar alias @ para mejor manera de importar los archivos

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AppRoutes } from './routes/AppRoutes'
+import { AppRoutes } from '@/routes/AppRoutes'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,6 +1,11 @@
 import { Route, Routes } from "react-router-dom"
-import App from "../App"
-import Index from "../pages/Index"
+import App from "@/App"
+import Index from "@/pages/Index"
+import TaskManagement from "@/pages/TaskManagement"
+import UpcomingTasks from "@/pages/UpcomingTasks"
+import UserProfile from "@/pages/UserProfile"
+import CompletedTasks from "@/pages/CompletedTasks"
+import Projects from "@/pages/Projects"
 
 export const AppRoutes = () => {
   return (
@@ -8,9 +13,13 @@ export const AppRoutes = () => {
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<Index />} />
+          <Route path="/tasks" element={<TaskManagement />} />
+          <Route path="/upcoming" element={<UpcomingTasks />} />
+          <Route path="/completed" element={<CompletedTasks />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/profile" element={<UserProfile />} />
         </Route>
       </Routes>
     </>
   )
-}
-
+} 
