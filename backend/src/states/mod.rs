a3e293eaef57326329db8855602beddf24e65c52
orlandotellez <espadachinkinno@gmail.com>
2026-03-12 22:53:25 -0600

    feat: actualizar cache de sqlx y agregar AppState

--- backend/src/states/mod.rs
+++ backend/src/states/mod.rs
@@ -1,3 +1,16 @@
+use redis::Client;
 use sqlx::PgPool;
 
 pub type DbState = PgPool;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db: DbState,
+    pub redis: Client,
+}
+
+impl AppState {
+    pub fn new(db: DbState, redis: Client) -> Self {
+        Self { db, redis }
+    }
+}
