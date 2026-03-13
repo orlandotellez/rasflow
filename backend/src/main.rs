c83dafa1c7b401bb7878ad626d531c17eaf727f5
orlandotellez <espadachinkinno@gmail.com>
2026-03-13 17:57:31 -0600

    feat: agregar cors configuración en el backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,11 +9,14 @@ mod routes;
 mod services;
 mod states;
 
-use axum::Router;
+use axum::{Router, http};
 use dotenvy::dotenv;
 use states::AppState;
 use tokio::net::TcpListener;
+use tower_http::cors::CorsLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+use crate::config::constants::FRONTEND_URL;
 
 const PORT: u16 = 3000;
 const HOST: &str = "0.0.0.0";
@@ -21,6 +24,18 @@ const HOST: &str = "0.0.0.0";
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let cors = CorsLayer::new()
+        .allow_origin(FRONTEND_URL.parse::<http::HeaderValue>().unwrap())
+        .allow_methods([
+            http::Method::GET,
+            http::Method::POST,
+            http::Method::PUT,
+            http::Method::PATCH,
+            http::Method::DELETE,
+        ])
+        .allow_headers([http::header::CONTENT_TYPE, http::header::AUTHORIZATION])
+        .allow_credentials(true);
 
     tracing_subscriber::registry()
         .with(
@@ -45,7 +60,7 @@ async fn main() {
 
     let app_state: AppState = AppState::new(db, redis);
 
-    let router: Router = routes::create_routes().with_state(app_state);
+    let router: Router = routes::create_routes().with_state(app_state).layer(cors);
 
     let addr: String = format!("{}:{}", HOST, PORT);
 
