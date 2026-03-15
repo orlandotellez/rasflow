ccf768851783baa078cc2b53d81f57f4972f2878
orlandotellez <espadachinkinno@gmail.com>
2026-03-14 22:38:37 -0600

    feat: agregar ruta para verificar si el servidor esta corriendo directamente desde /

--- backend/src/routes/v1/mod.rs
+++ backend/src/routes/v1/mod.rs
@@ -2,12 +2,13 @@ pub mod auth_routes;
 pub mod project_routes;
 pub mod task_routes;
 
-use axum::Router;
+use axum::{Router, routing::get};
 
 use crate::states::AppState;
 
 pub fn v1_routes() -> Router<AppState> {
     Router::new()
+        .route("/", get(|| async { "Server running" }))
         .merge(auth_routes::routes())
         .merge(project_routes::routes())
         .merge(task_routes::routes())
